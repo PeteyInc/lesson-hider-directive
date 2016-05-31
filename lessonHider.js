99c80698e7c60ba267e9f52baebe263415b406b4
Jared M. Peterson <jared@peteyinc.com>
2016-05-31 13:10:42 -0600

    step 3 done

--- lessonHider.js
+++ lessonHider.js
@@ -1,5 +1,9 @@
 angular.module('directivePractice').directive('lessonHider', function() {
   return {
-
+    restrict: 'E',
+    templateUrl: 'lessonHider.html',
+    link: function(scope, element, attributes) {
+      console.log(scope, element, attributes)
+    }
   };
 });
